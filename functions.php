491862241859e2bf01a8b7ef4a0c2207138ffc9e
Chris Mulvany <info@chrismulvany.com>
2025-02-06 13:32:00 +0000

    Took a static homepage and turned into WordPress homepage

--- functions.php
+++ functions.php
@@ -5,7 +5,11 @@ function university_files() {
     // From within this function you can add as many .css or .js files that you want to use.
 
     // To call a stylesheet or .css file, it looks for 2 parameters 1st a name for the stylesheet, 2nd a location that points to the file.
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());  
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');  
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');  
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     /**
      * Note: For the second parameter we use a WP function here to get the 'style.css' file. Later we will point to locations of files.
      * If you wished to add other Stylesheet files you would simply copy the line above for each file with the necessary parameters.
