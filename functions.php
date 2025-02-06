f0d7c81414e6a1b97b0c8dcc5eb23d971458e179
Chris Mulvany <info@chrismulvany.com>
2025-02-06 12:41:30 +0000

    Updated notes

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
+// Create our custom function university_files
 function university_files() {
     // From within this function you can add as many .css or .js files that you want to use.
 
-    // To call a stylesheet or .css file, it looks for 2 parameters 1. a name for the stylesheet, 2. a location that points to the file.
+    // To call a stylesheet or .css file, it looks for 2 parameters 1st a name for the stylesheet, 2nd a location that points to the file.
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());  
     /**
      * Note: For the second parameter we use a WP function here to get the 'style.css' file. Later we will point to locations of files.
@@ -12,5 +13,6 @@ function university_files() {
      */
 }
 
-// Add two parameters - The first specifies scripts such as .css or .js files.  The 2nd specifies a function to run that we create above.
+// Call out custom function university_files
 add_action('wp_enqueue_scripts', 'university_files');
+// Add two parameters - The first specifies scripts such as .css or .js files.  The 2nd specifies a function to run that we create above.
